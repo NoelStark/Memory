ef70a9844a8c5552a55ffd258a7b0c178f945f4a
NoelStark <noel.k.stark@gmail.com>
2024-09-20 14:20:58 +0200

    Added Hamburger menu to Gamemenu

--- Gamemenu/gamemenu.js
+++ Gamemenu/gamemenu.js
@@ -8,3 +8,21 @@ document.addEventListener(
     document.getElementById('settingsBtn').addEventListener('click', openSettings);
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerIcon = document.getElementById('hamburger-icon');
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+
+    hamburgerIcon.addEventListener("click", () =>{
+        if(hamburgerMenu.classList.contains('open')){
+            hamburgerMenu.classList.add('closing');
+            hamburgerMenu.classList.remove('open');
+        }
+        else{
+            hamburgerMenu.classList.remove('closing');
+            hamburgerMenu.classList.add('open');
+
+        }
+        
+    });
+});
