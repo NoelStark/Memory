a877debb75b584bedfa57c4bfc0a3844812e8857
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2024-09-21 14:16:42 +0200

    uppdaterad funktion för att kontrollera matchning

--- Gameboard/gameboard.js
+++ Gameboard/gameboard.js
@@ -13,4 +13,25 @@ function shuffle(array) {
 function createBoard() {
   const board = document.getElementsById('game-board');
   const shuffledCards = shuffle(cards);
+
+
+shuffledCards.forEach(cardValue => {
+  const cardElement = document.createElement('div');
+  cardElement.classList.add('card')
+  cardElement.dataset.value = cardValue;
+  cardElement.addEventListener('click', flipcard);
+  board.appendChild(cardElement);
+});
 }
+
+function flipCard() {
+  const card = this;
+  card.classList.add('flipped');
+  card.innerText = card.dataset.value;
+  flippedCards.push(card);
+
+  if (flippedCards.length === 2) {
+    checkMatch();
+  }
+}
+
