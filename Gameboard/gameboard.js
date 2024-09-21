ad71a78783c13ff4a13b3c7bcd1a73f5c6a0004c
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2024-09-21 14:56:52 +0200

    uppdaterad färgkod för gameboard och fungerande prototyp

--- Gameboard/gameboard.js
+++ Gameboard/gameboard.js
@@ -1,29 +1,31 @@
-// shär skapar jag spelkort
-const card = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ]
+// Korten som ska visas på spelbordet
+const cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
+// Håller reda på de kort som har vänds
 let flippedCards = [];
 let matchedCards = [];
 
-//blandar
-
+// Funktion för att blanda korten
 function shuffle(array) {
   return array.sort(() => 0.5 - Math.random());
 }
 
+// Funktion för att skapa spelbordet
 function createBoard() {
-  const board = document.getElementsById('game-board');
+  const board = document.getElementById('game-board');
   const shuffledCards = shuffle(cards);
 
-
-shuffledCards.forEach(cardValue => {
-  const cardElement = document.createElement('div');
-  cardElement.classList.add('card')
-  cardElement.dataset.value = cardValue;
-  cardElement.addEventListener('click', flipcard);
-  board.appendChild(cardElement);
-});
+  // Skapa och lägg till korten dynamiskt
+  shuffledCards.forEach(cardValue => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.dataset.value = cardValue;
+    cardElement.addEventListener('click', flipCard);
+    board.appendChild(cardElement);  // Lägg till kortet på spelbordet
+  });
 }
 
+// Funktion för att vända kortet
 function flipCard() {
   const card = this;
   card.classList.add('flipped');
@@ -35,3 +37,25 @@ function flipCard() {
   }
 }
 
+// Funktion för att kontrollera om korten matchar
+function checkMatch() {
+  const [card1, card2] = flippedCards;
+  if (card1.dataset.value === card2.dataset.value) {
+    matchedCards.push(card1, card2);
+    flippedCards = [];
+    if (matchedCards.length === cards.length) {
+      setTimeout(() => alert('You won!'), 500);
+    }
+  } else {
+    setTimeout(() => {
+      card1.classList.remove('flipped');
+      card1.innerText = '';
+      card2.classList.remove('flipped');
+      card2.innerText = '';
+      flippedCards = [];
+    }, 1000);
+  }
+}
+
+// När sidan laddas, skapa spelbordet
+document.addEventListener('DOMContentLoaded', createBoard);
